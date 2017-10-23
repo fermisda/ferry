57bb8fd09e8d1c29f150171513f19608856af675
Kenneth Herner <kherner@fermicloud138.fnal.gov>
2017-10-23 17:16:55 -0500

    first pass of TLS-enabled main server

--- API/src/main.go
+++ API/src/main.go
@@ -6,8 +6,9 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/gorilla/mux"
 	//"io"
-	//"time"
+	"time"
 	"net/http"
+	"crypto/tls"
 )
 
 var DBptr *sql.DB
@@ -32,16 +33,86 @@ func main () {
 			log.Fatal(pingerr)
 		}
 	}
-
+	
 	grouter := mux.NewRouter()
 	grouter.HandleFunc("/", handler)
-	grouter.HandleFunc("/getUserGroups", getUserGroups)
-	grouter.HandleFunc("/getUserInfo", getUserInfo)
-	grouter.HandleFunc("/getUserCertificateDNs", getUserCertificateDNs)
-	grouter.HandleFunc("/getUserFQANs", getUserFQANs)
-	grouter.HandleFunc("/getSuperUserList", getSuperUserList)
-	http.Handle("/", grouter)
-	http.ListenAndServe(":8080", nil)
 	
+	//user API calls
+	grouter.HandleFunc("/getUserCertificateDNs", getUserCertificateDNs)
+	grouter.HandleFunc("/getUserFQANs"	      , getUserFQANs)     
+	grouter.HandleFunc("/getSuperUserList"     , getSuperUserList)      
+	grouter.HandleFunc("/getUserGroups"	      , getUserGroups)	       
+	grouter.HandleFunc("/getUserInfo"          , getUserInfo)     
+	
+	//group API calls
+	grouter.HandleFunc("/getgroupmembers", getGroupMembers)
+	grouter.HandleFunc("/createGroup",                  createGroup)                 
+	grouter.HandleFunc("/deleteGroupt", 		    deleteGroupt)                
+	grouter.HandleFunc("/deleteGroup", 		    deleteGroup)                 
+	grouter.HandleFunc("/addGroupToUnit", 		    addGroupToUnit)              
+	grouter.HandleFunc("/removeGroupFromUnit", 	    removeGroupFromUnit)         
+	grouter.HandleFunc("/setPrimaryStatusGroup", 	    setPrimaryStatusGroup)       
+	grouter.HandleFunc("/removePrimaryStatusfromGroup", removePrimaryStatusfromGroup)
+	grouter.HandleFunc("/getGroupMembers", 		    getGroupMembers)             
+	grouter.HandleFunc("/IsUserLeaderOf", 		    IsUserLeaderOf)              
+	grouter.HandleFunc("/setGroupLeader", 		    setGroupLeader)              
+	grouter.HandleFunc("/removeGroupLeader", 	    removeGroupLeader)           
+	grouter.HandleFunc("/getGroupUnits", 		    getGroupUnits)               
+	grouter.HandleFunc("/getGroupBatchPriorities", 	    getGroupBatchPriorities)     
+	grouter.HandleFunc("/getGroupCondorQuotas", 	    getGroupCondorQuotas)        
+	grouter.HandleFunc("/setGroupBatchPriority", 	    setGroupBatchPriority)       
+	grouter.HandleFunc("/setGroupCondorQuota", 	    setGroupCondorQuota)         
+	grouter.HandleFunc("/getGroupStorageQuotas", 	    getGroupStorageQuotas)       
+	grouter.HandleFunc("/setGroupStorageQuota", 	    setGroupStorageQuota)        
+	grouter.HandleFunc("/setUserAccessToResource", 	    setUserAccessToResource)     
+	grouter.HandleFunc("/removeUserAccessFromResource", removeUserAccessFromResource)
+	// misc API cals
+	grouter.HandleFunc("/getPasswdFile",      getPasswdFile)       
+	grouter.HandleFunc("/getGroupFile", 	  getGroupFile)      
+	grouter.HandleFunc("/getGridmapFile", 	  getGridmapFile)    
+	grouter.HandleFunc("/getVORoleMapFile",   getVORoleMapFile)  
+	grouter.HandleFunc("/getUserUID", 	  getUserUID)        
+	grouter.HandleFunc("/getUserUname", 	  getUserUname)      
+	grouter.HandleFunc("/getGroupGID", 	  getGroupGID)       
+	grouter.HandleFunc("/getGroupName", 	  getGroupName)      
+	grouter.HandleFunc("/lookupCertificateDN",lookupCertificateDN)
+	//collaboration unit API calls
+	grouter.HandleFunc("/createCollaborationUnit",             createCollaborationUnit)           
+	grouter.HandleFunc("/removeCollaborationUnit", 		   removeCollaborationUnit)           
+	grouter.HandleFunc("/setCollaborationUnitInfo", 	   setCollaborationUnitInfo)          
+	grouter.HandleFunc("/getCollaborationUnitMembers",	   getCollaborationUnitMembers)       
+	grouter.HandleFunc("/getGroupsInCollaborationUnit", 	   getGroupsInCollaborationUnit)      
+	grouter.HandleFunc("/getGroupLeadersinCollaborationUnit",  getGroupLeadersinCollaborationUnit)
+	grouter.HandleFunc("/getCollaborationUnitStorageResources",getCollaborationUnitStorageResources)
+	grouter.HandleFunc("/getCollaborationUnitComputeResources",getCollaborationUnitComputeResources)
+	grouter.HandleFunc("/createFQAN",			   createFQAN)                        
+	grouter.HandleFunc("/removeFQAN",			   removeFQAN)                        
+	grouter.HandleFunc("/setFQANMappings",                     setFQANMappings)                    
+	
+	mainsrv := &http.Server{
+		Addr: ":8443",	
+		ReadTimeout: 10*time.Second,
+		Handler: grouter,
+	}	     
+	
+	var certstring = [1]string{"/etc/pki/tls/certs/ca-bundle.crt"}
+	var certslice []string = certstring[0:1]
+	certpool, err := loadCerts(certslice)
+	if err != nil {
+		log.Fatal(err)
+	}
+	mainsrv.TLSConfig = &tls.Config{
+		ClientAuth: tls.VerifyClientCertIfGiven,
+//tls.RequireAndVerifyClientCert,
+		ClientCAs:  certpool,
+	}
+	
+//	fmt.Println(certpool.Subjects())
+
+	serverror := mainsrv.ListenAndServeTLS("/etc/grid-security/hostcert.pem","/etc/grid-security/hostkey.pem")
+//	serverror := http.ListenAndServeTLS(":8443","/etc/grid-security/hostcert.pem","/etc/grid-security/hostkey.pem",nil)
+	if serverror != nil {
+		log.Fatal(serverror)
+	}
 	defer Mydb.Close()
 }
