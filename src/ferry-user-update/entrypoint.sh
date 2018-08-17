56e2d33e1ab2540c6aaf58f88a899f5370a5d260
Interface Monitor <ifmon@lssrv01.fnal.gov>
2018-08-17 12:39:11 -0500

    bad quoting

--- src/ferry-user-update/entrypoint.sh
+++ src/ferry-user-update/entrypoint.sh
@@ -2,9 +2,9 @@
 
 while /bin/true
 do
-   echo "Done sleeping.  Starting ferry-user-update.py run...
+   echo "Done sleeping.  Starting ferry-user-update.py run..."
    python36 /home/ferry/ferry-user-update.py -c /home/ferry/ferry-user-update.config
-   echo "Done with ferry-user-update.py run.  Sleeping....
+   echo "Done with ferry-user-update.py run.  Sleeping...."
    sleep 3600
 done
 
