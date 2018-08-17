a95e0594205d44d14d1912edf071d944d92d85c2
Interface Monitor <ifmon@lssrv01.fnal.gov>
2018-08-17 09:32:01 -0500

    deploying cron job for updates

--- src/ferry-user-update/entrypoint.sh
+++ src/ferry-user-update/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-#while /bin/true
-#do
-#   python3 /home/ferry/ferry-user-update.py -c /home/ferry/ferry-user-update.config
-#   sleep 3600
-#done
+while /bin/true
+do
+   python36 /home/ferry/ferry-user-update.py -c /home/ferry/ferry-user-update.config
+   sleep 3600
+done
 
-python -V
