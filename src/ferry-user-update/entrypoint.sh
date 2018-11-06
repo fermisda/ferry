0746b36fb47183754dc4d640bbdd863c8860cdaf
Interface Monitor <ifmon@lssrv01.fnal.gov>
2018-11-06 10:32:00 -0600

    changing update script to 15 minute loop

--- src/ferry-user-update/entrypoint.sh
+++ src/ferry-user-update/entrypoint.sh
@@ -5,6 +5,6 @@ do
    echo "Done sleeping.  Starting ferry-user-update.py run..."
    python36 /home/ferry/ferry-user-update.py -c /home/ferry/ferry-user-update.config
    echo "Done with ferry-user-update.py run.  Sleeping...."
-   sleep 3600
+   sleep 900
 done
 
