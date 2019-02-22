f21e3255f0dea0b29a2f669bd37e36037ae2d81d
Bruno Coimbra <coimbra@fnal.gov>
2019-02-22 17:09:09 -0600

    fix ferry-user-update directory regex

--- src/ferry-user-update/ferry-user-update.py
+++ src/ferry-user-update/ferry-user-update.py
@@ -59,7 +59,7 @@ class User:
 
     def addComputeAccess(self, accessString):
         accessString = accessString.replace("$USER", self.uname)
-        resource, group, home, shell, primary = re.findall(r"(\w+):([\w$]+),([\w\/\$]+),([\w\/\$]+),(true|false)", accessString)[0]
+        resource, group, home, shell, primary = re.findall(r"(\w+):([\w$]+),([\w\/\$\-]+),([\w\/\$\-]+),(true|false)", accessString)[0]
         self.compute_access[resource] = {
             "username": self.uname,
             "groupname": group,
@@ -100,7 +100,7 @@ class Group:
 
 def resources(resourcesString):
     resoruces = []
-    for resource, _ in re.findall(r"(\w+):[\w$]+,[\w\/\$]+,[\w\/\$]+,(true|false);", resourcesString):
+    for resource, _ in re.findall(r"(\w+):[\w$]+,[\w\/\$\-]+,[\w\/\$\-]+,(true|false);", resourcesString):
         resoruces.append(resource)
     return resoruces
 
