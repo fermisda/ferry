59ea9826273ce21615e073a12062102947dcdf1e
Bruno Coimbra <coimbra@fnal.gov>
2019-04-09 13:36:19 -0500

    fix ferry-user-update

--- src/ferry-user-update/ferry-user-update.py
+++ src/ferry-user-update/ferry-user-update.py
@@ -152,8 +152,10 @@ def fetch_userdb():
         userdbFile.write(urllib.request.urlopen(u).read().decode())
         userdbFile.close()
         if os.path.isfile(files[f] + ".cache"):
-            userdbLines = open(files[f], "r").readlines().sort()
-            cacheLines = open(files[f] + ".cache", "r").readlines().sort()
+            userdbLines = open(files[f], "r").readlines()
+            cacheLines = open(files[f] + ".cache", "r").readlines()
+            userdbLines.sort()
+            cacheLines.sort()
             s = difflib.SequenceMatcher(None, userdbLines, cacheLines)
             changeRatio = 1 - s.ratio()
             totalChangeRatio += changeRatio
