05d72d120b0760d2e46d434cbafcdebde1bfb295
Stephen P. White <swhite@fnal.gov>
2021-02-16 13:43:40 -0600

    Support for checking accessors now in DB.

--- server/src/baseAPI.go
+++ server/src/baseAPI.go
@@ -30,6 +30,17 @@ func (b BaseAPI) Run(w http.ResponseWriter, r *http.Request) {
 	var output Output
 	defer output.Parse(context, w)
 
+	// authorize needs a DB connection, so do this first.
+	var err error
+	context.DBtx, context.Ckey, err = LoadTransaction(r, DBptr)
+	if err != nil {
+		err := errors.New("error starting database transaction")
+		output.Err = append(output.Err, err)
+		log.WithFields(QueryFields(context)).Error(err)
+		return
+	}
+	defer context.DBtx.Rollback(context.Ckey)
+
 	authLevel, message := authorize(context, b.AccessRole)
 	context.AuthRole = b.AccessRole
 	context.AuthLevel = authLevel
@@ -40,16 +51,6 @@ func (b BaseAPI) Run(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	log.WithFields(QueryFields(context)).Debug(message)
-
-	var err error
-	context.DBtx, context.Ckey, err = LoadTransaction(r, DBptr)
-	if err != nil {
-		err := errors.New("error starting database transaction")
-		output.Err = append(output.Err, err)
-		log.WithFields(QueryFields(context)).Error(err)
-		return
-	}
-	defer context.DBtx.Rollback(context.Ckey)
 
 	input := make(Input)
 	parseErr := input.Parse(context, b.InputModel)
