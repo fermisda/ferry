be8caa64aa4c731678ed661c3b0c72fc02e3b8db
Stephen P. White <swhite@fnal.gov>
2022-05-13 11:11:46 -0500

    Adding script for syncing UUIDs across the databases.

--- ddl/v2.1.1a.sql
+++ ddl/v2.1.1a.sql
@@ -7,7 +7,3 @@ ALTER TABLE users ADD CONSTRAINT unq_users UNIQUE ( vopersonid );
 ALTER TABLE user_deletions ADD vopersonid uuid;
 
 
--- Now run syncLdapWithFerry which will update all the LDAP and Ferry records.
--- Then run this delete command
-
--- delete from external_affiliation_attribute where attribute = 'voPersonID';
