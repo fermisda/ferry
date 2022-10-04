b61e131875a6e5426e52efc69eb8d4b0c4d383fb
Stephen P. White <swhite@fnal.gov>
2022-10-04 12:47:43 -0500

    removed in_ldap field

--- ddl/v2.2.2.sql
+++ ddl/v2.2.2.sql
@@ -3,6 +3,3 @@ DROP TABLE "public".user_deletions CASCADE;
 
 DROP TABLE "public".user_group_deletions CASCADE;
 
-ALTER TABLE "public".users ADD in_ldap boolean DEFAULT false NOT NULL  ;
-
-update users set in_ldap=true where vopersonid is not null ;
